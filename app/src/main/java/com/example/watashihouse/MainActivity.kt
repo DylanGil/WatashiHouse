4ce066f64b4db73c7daf6cd90ca8fe1346e3f78a
DylanGil <dylan.flame.libra@gmail.com>
2022-01-12 21:25:32 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/watashihouse/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.watashihouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewTestView: TextView
+    private lateinit var buttonTestView: TextView
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        CreateElement()
+        SetButtonEvent()
+    }
+
+    fun CreateElement(){
+        textViewTestView = findViewById(R.id.textViewTest)
+        buttonTestView = findViewById<Button>(R.id.buttonTest)
+    }
+
+    fun SetButtonEvent(){
+        buttonTestView.setOnClickListener{
+            //monCode
+            textViewTestView.text = "monString"
+        }
+    }
+}
