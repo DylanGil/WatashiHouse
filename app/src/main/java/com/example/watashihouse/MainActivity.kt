9b50fd5d4c2223015d09950679ca707c01d95962
Dylan GIL AMARO <dylan.flame.libra@gmail.com>
2022-01-24 16:34:45 +0100

    start with bottomNav

--- app/src/main/java/com/example/watashihouse/MainActivity.kt
+++ app/src/main/java/com/example/watashihouse/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.watashihouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textViewTestView: TextView
     private lateinit var buttonTestView: TextView
+    private lateinit var bottom_navigation_view: View
 
 
 
@@ -18,12 +20,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         CreateElement()
-        SetButtonEvent()
+        //SetButtonEvent()
+        teeest()
+
+        //BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
     }
 
+    fun teeest(){
+        bottom_navigation_view = findViewById(R.id.bottom_navigation)
+        bottom_navigation_view.setOnClickListener{
+            when(R.id.bottom_navigation){
+                R.id.nav_home -> HomeFragment()
+                R.id.nav_favorite -> FavorisFragment()
+            }
+        }
+    }
+
+
     fun CreateElement(){
-        textViewTestView = findViewById(R.id.textViewTest)
-        buttonTestView = findViewById<Button>(R.id.buttonTest)
+        //textViewTestView = findViewById(R.id.textViewTest)
+        //buttonTestView = findViewById<Button>(R.id.buttonTest)
     }
 
     fun SetButtonEvent(){
