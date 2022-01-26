0e85cd2de0219474033881b796ce926d0689b1f7
DylanGil <dylan.flame.libra@gmail.com>
2022-01-26 18:47:20 +0100

    BottomNavigation Work !!

--- app/src/main/java/com/example/watashihouse/MainActivity.kt
+++ app/src/main/java/com/example/watashihouse/MainActivity.kt
@@ -2,44 +2,50 @@ package com.example.watashihouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textViewTestView: TextView
     private lateinit var buttonTestView: TextView
-    private lateinit var bottom_navigation_view: View
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         CreateElement()
         //SetButtonEvent()
-        teeest()
-
-        //BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
     }
-
-    fun teeest(){
-        bottom_navigation_view = findViewById(R.id.bottom_navigation)
-        bottom_navigation_view.setOnClickListener{
-            when(R.id.bottom_navigation){
-                R.id.nav_home -> HomeFragment()
-                R.id.nav_favorite -> FavorisFragment()
-            }
-        }
-    }
-
 
     fun CreateElement(){
         //textViewTestView = findViewById(R.id.textViewTest)
         //buttonTestView = findViewById<Button>(R.id.buttonTest)
+
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val favorisFragment = FavorisFragment()
+        val shoppingCartFragment = ShoppingCartFragment()
+        val profileFragment = ProfileFragment()
+        makeCurrentFragment(homeFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> makeCurrentFragment(homeFragment)
+                R.id.nav_search -> makeCurrentFragment(searchFragment)
+                R.id.nav_favorite -> makeCurrentFragment(favorisFragment)
+                R.id.nav_cart -> makeCurrentFragment(shoppingCartFragment)
+                R.id.nav_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
     }
 
     fun SetButtonEvent(){
