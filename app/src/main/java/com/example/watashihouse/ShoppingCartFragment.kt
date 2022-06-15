0a35d034de67b7024cb66400e716043b2d29d6bf
DylanGil <dylan.flame.libra@gmail.com>
2022-06-15 21:37:30 +0200

    Vider le panier OK

--- app/src/main/java/com/example/watashihouse/ShoppingCartFragment.kt
+++ app/src/main/java/com/example/watashihouse/ShoppingCartFragment.kt
@@ -1,24 +1,32 @@
 package com.example.watashihouse
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.clear
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.auth0.android.jwt.JWT
 import com.example.watashihouse.databinding.FragmentShoppingCartBinding
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import kotlin.collections.Collection
 
 class ShoppingCartFragment : Fragment() {
@@ -27,8 +35,9 @@ class ShoppingCartFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var dataStore: DataStore<Preferences>
     lateinit var recyclerViewMeuble: RecyclerView
-    private lateinit var deleteLocalStorageButton: Button
+    private lateinit var deleteShoppingCartButton: Button
     private lateinit var totalNumberText: TextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,14 +47,30 @@ class ShoppingCartFragment : Fragment() {
 
         _binding = FragmentShoppingCartBinding.inflate(inflater, container, false)
         var view = inflater.inflate(R.layout.fragment_shopping_cart, container, false)
+
+        var userId = "0"
+        dataStore = context?.createDataStore(name = "jwt")!!
         recyclerViewMeuble = view.findViewById(R.id.recyclerViewShoppingCart) as RecyclerView
         totalNumberText = view.findViewById(R.id.totalNumberText) as TextView
-        deleteLocalStorageButton = view.findViewById(R.id.deleteLocalStorageButton)
-        deleteLocalStorageButton.setOnClickListener {
-            Toast.makeText(context, "Le panier a bien été vidé", Toast.LENGTH_SHORT).show()
-            lifecycleScope?.launch{
-                clearLocalStorage(dataStore)
-            }
+        deleteShoppingCartButton = view.findViewById(R.id.deleteShoppingCartButton)
+        deleteShoppingCartButton.setOnClickListener {
+            val retro = Retro().getRetroClientInstance().create(WatashiApi::class.java)
+            retro.deleteAllProductsFromShoppingCart(userId).enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                    Toast.makeText(context, "Le panier a bien été vidé", Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.e("Error", t.message.toString())
+                    Toast.makeText(context, "Erreur: Redémarrer l'application", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+        lifecycleScope?.launch{
+            var jwt = readfromLocalStorage("jwt")?.let { JWT(it) }
+            userId = jwt?.getClaim("id")?.asString().toString()
         }
 
         primaryFunction()
@@ -53,16 +78,21 @@ class ShoppingCartFragment : Fragment() {
     }
 
     private fun primaryFunction(){
-        dataStore = context?.createDataStore(name = "meubleStored")!!
+
 
         lifecycleScope?.launch{
-            readFromLocalStorage()
-
+            //readFromLocalStoragee()
         }
 
     }
 
-    private suspend fun readFromLocalStorage(){
+    private suspend fun readfromLocalStorage(key: String): String? {
+        val dataStoreKey = preferencesKey<String>(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
+
+    private suspend fun readFromLocalStoragee(){
         val listOfMeuble = mutableListOf<MeubleDeleteButton>()
         var prixTotalPanier = 0.0
 
@@ -94,12 +124,4 @@ class ShoppingCartFragment : Fragment() {
         }
 
     }
-
-    private suspend fun clearLocalStorage(dataStore: DataStore<Preferences>){
-        dataStore.edit {
-            it.clear()
-        }
-    }
-
-
 }
--- app/src/main/java/com/example/watashihouse/WatashiApi.kt
+++ app/src/main/java/com/example/watashihouse/WatashiApi.kt
@@ -1,10 +1,12 @@
 package com.example.watashihouse
 
+import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.Response
+import retrofit2.http.*
+
 
 interface WatashiApi {
     @POST("utilisateurs/connexion")
@@ -14,6 +16,11 @@ interface WatashiApi {
 
     @GET("/articles?sortBy=name")
     fun getAllProducts(): Call<JsonObject>
+
+    @DELETE("/paniers/{userId}/supprimerArticle")
+    fun deleteAllProductsFromShoppingCart
+                (@Path("userId") userId: String): Call<ResponseBody>
+
 
     //@GET("/user/{id}")
     //suspend fun getUser(@Path("id") id: String): Response<User>
